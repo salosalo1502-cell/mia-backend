f33bcda690dcf210dd44f7c5f176eb1ee64b0f78
salosalo1502-cell <salosalo1502@gmail.com>
2026-09-16 11:42:23 +0400

    Update mail.js

--- config/mail.js
+++ config/mail.js
@@ -4,6 +4,7 @@ const transporter = nodemailer.createTransport({
   host: 'smtp.gmail.com',
   port: 587,
   secure: false,
+  family: 4,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
@@ -11,3 +12,5 @@ const transporter = nodemailer.createTransport({
 });
 
 export default transporter;
+
+
